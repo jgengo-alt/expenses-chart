883d7517710847f4364858b272339add9de8803e
jgengo <jgengo@users.noreply.github.com>
2022-06-12 13:19:16 +0000

    deploy: 2721ba370b7537a35895c722457339e09ee7733f

---
+++ _next/static/qKkWRmpDcJ5URnOClgHn0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ddb4c5c78941ad05.js"],"/_error":["static/chunks/pages/_error-68c8fb2ff52d9081.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
